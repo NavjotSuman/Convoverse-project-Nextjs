a804f8bd020e17df8e77f17ab2a9e706d6d863e4
Navjot Suman <navjotsumandev@gmail.com>
2024-07-24 15:32:01 +0530

    fix error of &apos;

--- src/components/Home/chat-placeholder.tsx
+++ src/components/Home/chat-placeholder.tsx
@@ -9,10 +9,10 @@ const ChatPlaceHolder = () => {
       <div className="flex flex-col items-center w-full justify-center pb-10 pt-32 gap-4">
         <Image src={"/Converse.png"} alt="Hero" width={181} height={181} />
         <p className="text-3xl mt-4 font-extralight mb-2">
-          Welcome to Convoverse! Let's get you connected.
+          Welcome to Convoverse! Let&apos;s get you connected.
         </p>
         <p className="w-1/2 text-center text-gray-primary text-sm text-muted-foreground">
-          See who's online and start a chat or video call! Whether it's catching up with a friend or collaborating on a project, Convoverse makes it easy to connect
+          See who&apos;s online and start a chat or video call! Whether it&apos;s catching up with a friend or collaborating on a project, Convoverse makes it easy to connect
         </p>
 
         {/* <Link href="https://github.com/navjotsuman/" className="rounded-full my-5 bg-green-primary hover:bg-green-secondary">
